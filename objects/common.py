fcd6da888ac6bc0c6f378de30d2b8ef1e96c9acf
Sebastián <57805712+elpollazo@users.noreply.github.com>
2021-07-14 21:08:09 -0400

    Update common.py

--- objects/common.py
+++ objects/common.py
@@ -3,6 +3,7 @@ import json
 __config = None
 
 def config():
+    """This function loads the parameters in ./objects/config.json."""
     global __config
     if not __config:
         with open('./objects/config.json') as f:
